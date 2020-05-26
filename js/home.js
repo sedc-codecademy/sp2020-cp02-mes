9b3a84b764e36fac577ea1f03f45b374c929b053
Darko <fotinovskidarko@gmail.com>
2020-05-26 12:09:52 +0200

    header v.1.5 / style updates

--- js/home.js
+++ js/home.js
@@ -1,3 +1,35 @@
+//***HEADER***
+
+const navSlide = () =>{
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+  
+
+    burger.addEventListener('click',()=>{
+          //toggle nav
+        nav.classList.toggle('nav-active');
+    
+    //animate links
+    navLinks.forEach((link, index)=>{
+        if(link.style.animation) {
+            link.style.animation = '';
+        }   else {
+            link.style.animation = `navLinkFade 0.8s ease forwards ${index/ 7 + 0.5}s`;
+        }
+
+        });
+        //burger animation
+
+        burger.classList.toggle('toggle');
+    
+    });
+}
+
+navSlide();
+
+
+
 //***Home slider***
 
 const carouselSlide = document.querySelector('.carousel-slide');
