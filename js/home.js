fb883e24599871c273e8755ac8dbf863e6d4bec2
Darko <fotinovskidarko@gmail.com>
2020-05-31 18:28:22 +0200

    latestNews slider is alive

--- js/home.js
+++ js/home.js
@@ -44,33 +44,95 @@ setInterval(function(){
 //************************************************************//
 
 var latestNews = [
-    "img/news1",
-    'img/news2',
-    'img/news3',
-    'img/news4',
-    'img/news5',
-    'img/news6',
-    'img/news7'
+    './img/606641.jpg',
+    './img/606641.jpg',
+    './img/606641.jpg',
+    './img/606641.jpg',
+    './img/606641.jpg',
+    './img/606641.jpg'
 ]
 
-document.getElementById("news3").setAttribute("alt", latestNews[2])
+function initializeNewsSlider() {
+    const newsBoxes = document.querySelectorAll(".news-slider > img")
+    var i;
+    for(i = 0; i < newsBoxes.length; i ++) {
+        newsBoxes[i].setAttribute("src", latestNews[i])
+    }
+
+}
+initializeNewsSlider();
 
 var newArray = []
 
-// function moveRight() {
-//     const firstElement = latestNews.shift();
-
-//     latestNews.push(firstElement)
-
-//     document.getElementById("news3").setAttribute("alt", latestNews[2])
-
-// }
-
 function moveLeft() {
 
+    const leftButton = document.getElementById("moveLeftBtn")
+    const rightButton = document.getElementById("moveRightBtn")
+    leftButton.classList.add("disable-events")
+    rightButton.classList.add("disable-events")
+
+    triggerAnimationLeft()
     const firstElement = latestNews.shift();
     latestNews.push(firstElement)
 
-    document.getElementById("news3").setAttribute("alt", latestNews[2])
+    setTimeout(function(){
+        initializeNewsSlider();
+        leftButton.classList.remove("disable-events")
+        rightButton.classList.remove("disable-events")
+    }, 1000)
+
+}
+
+function moveLeftTwice(){
+    moveLeft()
+    setTimeout(moveLeft, 1000)
+}
+
+function moveRightTwice(){
+    moveRight()
+    setTimeout(moveRight, 1000)
+}
+function moveRight() {
+
+    const leftButton = document.getElementById("moveLeftBtn")
+    const rightButton = document.getElementById("moveRightBtn")
+    leftButton.classList.add("disable-events")
+    rightButton.classList.add("disable-events")
+
+    triggerAnimationRight()
+    const lastElement = latestNews[latestNews.length-1];
+    latestNews = [lastElement].concat(latestNews.slice(0, latestNews.length-1))
+
+    setTimeout(function(){
+        initializeNewsSlider();
+        leftButton.classList.remove("disable-events")
+        rightButton.classList.remove("disable-events")
+    }, 1000)
+}
+
+function triggerAnimationLeft() {
+
+    const newsBoxes = document.querySelectorAll(".news-slider > img")
+
+    var i;
+    for(i = 0; i < newsBoxes.length; i ++) {
+        newsBoxes[i].classList.remove('slider' + (i + 1) + '-animate-right'); // reset animation
+        newsBoxes[i].classList.remove('slider' + (i + 1) + '-animate-left'); // reset animation
+        void newsBoxes[i].offsetWidth; // trigger reflow
+        newsBoxes[i].classList.add('slider' + (i + 1) + '-animate-left'); // start animation
+    }
+
+}
+
+function triggerAnimationRight() {
+    const newsBoxes = document.querySelectorAll(".news-slider > img")
+
+    var i;
+    for(i = 0; i < newsBoxes.length; i ++) {
+        newsBoxes[i].classList.remove('slider' + (i + 1) + '-animate-right'); // reset animation
+        newsBoxes[i].classList.remove('slider' + (i + 1) + '-animate-left'); // reset animation
+        void newsBoxes[i].offsetWidth; // trigger reflow
+        newsBoxes[i].classList.add('slider' + (i + 1) + '-animate-right'); // start animation
+    }
 
 }
