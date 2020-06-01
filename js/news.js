87e753cf57966df187eef5bef5e582263344f2c1
goranTasev <60360981+goranTasev@users.noreply.github.com>
2020-06-01 13:25:02 +0200

    news

--- js/news.js
+++ js/news.js
@@ -1,9 +1,1 @@
-document.getElementById("btn-readMore").addEventListener("click", function(){
-    let readMoreDiv = document.getElementsById();
-    if(readMoreDiv.display == "none") {
-        readMoreDiv.display == "block";
-    } 
-    else {
-        readMoreDiv.display == "none"
-    }
-});
+
