9b97ee0b7f0b9252f0ea90075b25423718b4b879
Darko Petrov <daremc2123@gmail.com>
2020-06-12 03:36:00 +0200

    about -> structure modified

--- js/about.js
+++ js/about.js
@@ -1,8 +1,15 @@
-let popupDiv = document.querySelectorAll(".structure__popUp");
-let structureMembers = document.querySelectorAll(".li__divWrapper");
+// let popupDiv = document.querySelectorAll(".structure__popUp");
+// let structureMembers = document.querySelectorAll(".li__divWrapper");
+let structureAtags = document.querySelectorAll(".atag100");
 
-$(popupDiv).hide();
-$(structureMembers).on("click", function(evt){
-    evt.preventDefault();
-    $(popupDiv).show();
-});
+for(let i = 0; i < structureAtags.length; i++) {
+    structureAtags[i].addEventListener('click', function(event) {
+        event.preventDefault();
+    })
+}
+
+// $(popupDiv).hide();
+// $(structureMembers).on("click", function(evt){
+//     evt.preventDefault();
+//     $(popupDiv).show();
+// });
